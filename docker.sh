5ce302902b8e0beb8d9b994a154904f6b1faf111
Nilesh Londhe <nilesh@cloudgeni.us>
2019-07-23 12:07:51 -0700

    usermod -aG docker cloudgenius

--- docker.sh
+++ docker.sh
@@ -12,5 +12,8 @@ apt-get update && \
       stable" && \
     apt-get update && \
     apt-get -y install docker-ce docker-ce-cli containerd.io
+
+usermod -aG docker cloudgenius # replace cloudgenius with your username
+
 curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
