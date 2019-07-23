79b80f64b7b1fa8afe07e3b168010f04bde4a9ad
Nilesh Londhe <nilesh@cloudgeni.us>
2019-07-22 20:31:15 -0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -12,5 +12,5 @@ apt-get update && \
       stable" && \
     apt-get update && \
     apt-get -y install docker-ce docker-ce-cli containerd.io
-curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
