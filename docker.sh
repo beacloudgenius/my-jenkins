90063ac8eddf213130f09b7751e53583326544eb
Nilesh Londhe <nilesh@cloudgeni.us>
2019-07-25 08:55:43 -0700

    edits

--- docker.sh
+++ docker.sh
@@ -13,7 +13,7 @@ apt-get update && \
     apt-get update && \
     apt-get -y install docker-ce docker-ce-cli containerd.io
 
-usermod -aG docker cloudgenius # replace cloudgenius with your username
+usermod -aG docker eeshan  # replace eeshan or cloudgenius with your username
 
 curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
